0cdaffd81e07496e47908e4e1dc3dcb38c8e58f0
rayhannn11 <rayhan.onepiece@gmail.com>
2024-04-05 13:22:16 +0700

    finishing 2

--- app/models/Admin.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const AdminSchema = new mongoose.Schema({
-  name: { type: String, required: true, maxlength: 24 },
-  email: { type: String, required: true, unique: true, maxlength: 36 },
-  level: { type: Number, default: 0, maxlength: 2 },
-});
-
-export default mongoose.models.Admin || mongoose.model('Admin', AdminSchema);
